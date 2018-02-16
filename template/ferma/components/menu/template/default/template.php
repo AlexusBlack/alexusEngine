84d9814b79043fee92be3a70aab9ca50c21bb4f4
Alex Chernov <alexusblack@gmail.com>
2018-02-16 16:45:05 +1100

    this is not engine, but prototype of it

--- template/ferma/components/menu/template/default/template.php
+++
@@ -1,7 +0,0 @@
-<?php
-foreach ($TPL_VALUES as $menuItem) {
-?>
-<a href="<?php print $menuItem['src'];?>" class="button"><?php print $menuItem['text'];?></a>
-<?php
-}
-?>
--- template/ferma/components/menu/template/ferma_footmenu/template.php
+++
@@ -1,7 +0,0 @@
-<div id="footmenu">
-	<ul>
-<?php foreach ($TPL_VALUES as $menuItem): ?>
-	<li><a href="<?php print $menuItem['src'];?>"><?php print $menuItem['text'];?></a></li>
-<?php endforeach; ?>
-	</ul>
-</div>
